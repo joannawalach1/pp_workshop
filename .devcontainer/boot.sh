81cbc826a7b9a3d3745e337526aa83f8e7e7b8f9
Gosia Sowańska <gosiasowanska@gmail.com>
2024-05-24 16:59:20 +0200

    Update boot.sh

--- .devcontainer/boot.sh
+++ .devcontainer/boot.sh
@@ -3,14 +3,7 @@
 echo "Updating RubyGems..."
 gem update --system -N
 
-echo "Installing dependencies..."
-bundle install
-yarn install
-
-echo "Copying database.yml..."
-cp -n config/database.yml.example config/database.yml
-
-echo "Creating database..."
-bin/rails db:prepare
+echo "Installing Rails..."
+gem install rails -v 7.1.3.2
 
 echo "Done!"
